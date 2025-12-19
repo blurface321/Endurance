ba59f7d8a74a6cb2bb6a087f254879400ead746e
Siddhant S Mishrikoti <siddhantsmishrikoti@Siddhants-MacBook-Air.local>
2025-12-19 23:08:37 +0530

    Tracking part done

--- src/app/tracking/tracking.ts
+++ src/app/tracking/tracking.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 export interface CardData {
+  cardName: string
   backgroundUrl: string; 
   innerSvgs: string[];   
   title: string;         
@@ -16,33 +17,36 @@ export interface CardData {
 export class Tracking {
   cards: CardData[] = [
     {
+      cardName: 'Calories',
       backgroundUrl: '../../../assets/images/bg7.png',
       innerSvgs: [
         '../../../assets/svg_icons/Calories.svg',
         '../../../assets/svg_icons/HiddenItems.svg',
-        '../../../assets/svg_icons/HealthScore.svg'
+        '../../../assets/svg_icons/HealthScore.svg',
       ],
       title: 'Monitor your calories',
-      description: 'Track your daily nutritional intake with precise macronutrient breakdowns.'
+      description: 'Track macronutrients and uncover hidden ingredients like oils or creams. Use the Health Score to instantly gauge the overall nutritional quality of your meals.'
     },
     {
+      cardName: 'Sleep',
       backgroundUrl: '../../../assets/images/bg6.png',
       innerSvgs: [
         '../../../assets/svg_icons/SleepImpact.svg',
         '../../../assets/svg_icons/SleepInsights.svg',
-        '../../../assets/svg_icons/SleepImpactBreakdown.svg'
+        '../../../assets/svg_icons/SleepImpactBreakdown.svg',
       ],
       title: 'Get a sleep impact score',
-      description: 'Analyze how your diet and activity levels affect your nightly rest and recovery.'
+      description: 'Analyze how meals affect Deep Sleep and REM with a predictive Sleep Impact score. Review specific nutrient helpers and disruptors to optimize your nightly rest.'
     },
     {
+      cardName: 'Gut',
       backgroundUrl: '../../../assets/images/bg4.png',
       innerSvgs: [
         '../../../assets/svg_icons/GutScore.svg',
-        '../../../assets/svg_icons/GutInsights.svg' 
+        '../../../assets/svg_icons/GutInsights.svg',
       ],
       title: 'Control your gut health',
-      description: 'Receive personalized meal suggestions to optimize your microbiome diversity.'
+      description: 'Monitor your Gut Support Score to identify ingredient boosters and disruptors. Track plant diversity and digestion forecasts to improve microbiome health.'
     }
   ];
 }
